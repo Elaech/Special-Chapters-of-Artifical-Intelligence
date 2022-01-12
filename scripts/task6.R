c69fb93990a9379450de0af7498fd6cf0f1c1514
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2022-01-12 15:03:10 +0200

    Update task6.R

--- scripts/task6.R
+++ scripts/task6.R
@@ -1,6 +1,9 @@
 library(devtools)
 install_github("vqv/ggbiplot")
 library(ggbiplot)
+library(factoextra)
+library(rgl)
+set.seed(42)
 
 money_dataset = function(){
   raw_dataset = read.csv("D:/Coding/Master/AI/Special-Chapters-of-Artifical-Intelligence/input/task6/bancnotes.dat",
@@ -12,12 +15,14 @@ money_dataset = function(){
 
 MONEY = money_dataset()
 
-MONEY.pca = prcomp(MONEY,scale= TRUE)
+MONEY.pca = prcomp(MONEY,scale. = TRUE)
 MONEY.pca
 summary(MONEY.pca)
+fviz_eig(MONEY.pca)
+cl <- kmeans(MONEY.pca$x[,1:2],2)
+MONEY.cluster2d<- as.factor(cl$cluster)
+ggbiplot(MONEY.pca,choices=c(1,2), col=MONEY.cluster2d)
 
-
-ggbiplot(MONEY.pca,choices=c(1,2))
-ggbiplot(MONEY.pca,choices=c(1,3))
-ggbiplot(MONEY.pca, choices=c(2,3))
-
+cl <- kmeans(MONEY.pca$x[,1:3],2)
+MONEY.cluster3d <- as.factor(cl$cluster)
+plot3d(MONEY.pca$x[,1:3], col=MONEY.cluster3d, main="k-means clusters", size=7)
